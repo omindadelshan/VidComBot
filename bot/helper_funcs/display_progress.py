1716051788eb406320e6e7ae09c2917b344248c8
safone1 <77989182+safone1@users.noreply.github.com>
2021-02-03 22:33:24 +0600

    Update display_progress.py

--- bot/helper_funcs/display_progress.py
+++ bot/helper_funcs/display_progress.py
@@ -49,12 +49,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \n📊 <b>Progress:</b> {2}%\n".format(
+        progress = "[{0}{1}] \n\n📊 <b>Progress:</b> {2}%\n".format(
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + "{0} of {1}\n🚀SPEED: {2}/s\n⏰S1 ETA: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
